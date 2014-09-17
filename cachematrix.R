fc94a00e1ec15b51928c8d4e8f44ecd79ac7daf4
Fredin <esteban.fredin@gmail.com>
2014-09-17 15:53:32 -0500

    modified comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,13 @@
 ## The following two functions: makeCacheMatrix and cacheSolve offer a way of
-## storing (cacheing)the inverse of a invertible square matrix in R's global
-## enviorment. It can spare the resource intensive process of re-inverting a
-## matrix every time it is needed.
+## cacheing the inverse of a numeric or complex invertible square matrix in
+## R's global environment. It can speed things up by sparing the resource
+## intensive process of re-inverting a matrix every time it's needed.
 
 ## makeCacheMatrix function creates a special "matrix" object that can cache
-## its inverse.
+## its inverse. It's argument must be an invertible numeric or complex matrix.
+## It outputs a list of functions that allow the user to set and retrieve the
+## values of the matrix and its inverse.
+
 makeCacheMatrix<- function (x = matrix()) {
   im <- NULL
   set <- function(y) {
@@ -19,10 +22,11 @@ makeCacheMatrix<- function (x = matrix()) {
        getinverse = getinverse)
 }
 
-## This function computes the inverse of the special "matrix" returned
-## by makeCacheMatrix above. If the inverse has already been calculated
-## (and the matrix has not changed), then cacheSolve should retrieve the 
-## inverse from the cache.
+## This function computes the inverse of the special "matrix" returned by
+## makeCacheMatrix above. If the inverse has already been calculated (and the
+## matrix has not changed), then cacheSolve should retrieve the inverse from
+## the cache.
+ 
 cacheSolve<- function (x,...){
   im <- x$getinverse()
   if(!is.null(im)) {
